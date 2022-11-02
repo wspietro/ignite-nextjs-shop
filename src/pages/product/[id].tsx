0ff74e8a976c679c65bffd98aff742c9840a76f9
Pietro Weg <pietro.sera@compasso.com.br>
2022-11-02 19:51:11 -0300

    :construction: feat: iniciando file-system-routing

---
+++ src/pages/product/[id].tsx
@@ -0,0 +1,9 @@
+import { useRouter } from 'next/router'
+
+export default function Product() {
+  const { query } = useRouter();
+
+  return (
+    <div>Product: {JSON.stringify(query)}</div>
+  )
+}
