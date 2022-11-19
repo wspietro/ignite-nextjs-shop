325f4f21fb1bc316b87557dac9eb592581b643d4
Pietro Weg <pietro.sera@compasso.com.br>
2022-11-19 18:42:03 -0300

    :sparkles: feat: configurando stitches

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { getCssText } from '../styles'
 
 export default function Document() {
   return (
@@ -7,6 +8,12 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+        {/* 
+        * Quando o usuário carregar a página, pelo lado do servidar, o cógio css necessário será retornado dessa função; 
+        * Stitches por padrao renderiza o código em run time;
+        * Dessa forma, temos css em ssr
+        */}
+        <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
       </Head>
       <body>
         {/* Em qual lugar da minha app vão os conteúdos das páginas */}
