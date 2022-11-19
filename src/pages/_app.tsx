e4eac994b2f08b48449b2d212c52c51d48b8819c
Pietro Weg <pietro.sera@compasso.com.br>
2022-11-19 19:07:05 -0300

    :sparkles: feat: cabecalho criado

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,24 @@
 
-// _app funciona como um container para as páginas da nossa aplicação.
+// _app funciona como um container para as páginas da nossa aplicação;
+// Ele é carregado em todas as páginas.
 
 import { AppProps } from "next/app"
 import { globalStyles } from "../styles/global"
 
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from '../styles/pages/app'
+
 globalStyles(); // fica fora, já que os estilos globais não mudam de acordo com a página;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="Logo ignite shop" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
 
