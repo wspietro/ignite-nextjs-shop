de1f02f9836330e9ff5f4dcaaa397949e86d3523
Pietro Weg <pietro.sera@compasso.com.br>
2022-11-19 20:42:06 -0300

    :construction: feat: manipulacao de assets com next

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,13 +8,15 @@ import { globalStyles } from "../styles/global"
 import logoImg from '../assets/logo.svg'
 import { Container, Header } from '../styles/pages/app'
 
+import Image from 'next/image';
+
 globalStyles(); // fica fora, já que os estilos globais não mudam de acordo com a página;
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="Logo ignite shop" />
+        <Image src={logoImg} alt="Logo ignite shop" />
       </Header>
 
       <Component {...pageProps} />
