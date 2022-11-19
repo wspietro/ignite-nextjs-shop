6ef1b2a78626624148ac30f9a044c5e90a93332c
Pietro Weg <pietro.sera@compasso.com.br>
2022-11-19 18:51:18 -0300

    :listick: feat: estilos globais

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 
-function MyApp({ Component, pageProps }) {
+// _app funciona como um container para as páginas da nossa aplicação.
+
+import { AppProps } from "next/app"
+import { globalStyles } from "../styles/global"
+
+globalStyles(); // fica fora, já que os estilos globais não mudam de acordo com a página;
+
+export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,4 +1,4 @@
-import { createStitches } from '@stitches/react'
+import { createStitches } from "@stitches/react";
 
 export const {
   config,
@@ -8,11 +8,19 @@ export const {
   keyframes,
   getCssText,
   theme,
-  createTheme
+  createTheme,
 } = createStitches({
   theme: {
     colors: {
-      rocketseat: '#8257e6'
+      white: '#FFF',
+
+      gray900: '#121214',
+      gray800: '#202024',
+      gray300: '#c4c4cc',
+      gray100: '#e1e1e6',
+
+      green500: '#00875f',
+      green300: '#00b37e',
     }
   }
 })
