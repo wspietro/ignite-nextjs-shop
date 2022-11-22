afed27faf284bbc47741be3ed94a6d8b202cab50
Pietro Weg <pietro.sera@compasso.com.br>
2022-11-21 23:42:46 -0300

    :construction: feat: product-page estrutura inicial

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,28 @@
 import { useRouter } from 'next/router'
 
+import { ImageContainer, ProductContainer, ProductDetails } from '../../styles/pages/product'
+
 export default function Product() {
   const { query } = useRouter();
 
   return (
-    <div>Product: {JSON.stringify(query)}</div>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta x</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur, quod. Architecto debitis aperiam, aspernatur doloremque vitae dolorum similique illum. A explicabo deserunt dignissimos aliquam et dolores optio nesciunt esse reiciendis.
+        </p>
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
